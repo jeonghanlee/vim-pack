c5cd69167a9950da6c2f8b2c720e4c8a403b01fd
Jeong Han Lee <jeonghan.lee@gmail.com>
2021-07-01 11:39:41 -0700

    fixed

--- add_mod.bash
+++ add_mod.bash
@@ -8,6 +8,6 @@ SC_TOP="$( cd -P "$( dirname "$SC_SCRIPT" )" && pwd )"
 module_url="$1";shift;
 target_path="$1";shift;
 
-git submodule add "$module_url" "${SC_TOP}/plugins/start/${target_path}"
+git submodule add "$module_url" "plugins/start/${target_path}"
 
 exit
