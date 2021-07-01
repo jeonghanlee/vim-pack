426ca72868353b48a5376082c65d77dc9ec5a17f
Jeong Han Lee <jeonghan.lee@gmail.com>
2021-07-01 12:34:53 -0700

    update init

--- init.bash
+++ init.bash
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+declare -g SC_SCRIPT;
+declare -g SC_TOP;
+SC_SCRIPT=${BASH_SOURCE[0]:-${0}}
+SC_TOP="$( cd -P "$( dirname "$SC_SCRIPT" )" && pwd )"
 
 git submodule update --init ./
-ln -snf .vimrc ~/.vimrc
+ln -snf "${SC_TOP}/.vimrc" "${HOME}/.vimrc"
