f010f926bf3786c796df2365789ee554922ace98
Jeong Han Lee <jeonghan.lee@gmail.com>
2022-08-23 09:30:08 -0700

    add taglist

--- add_mod.bash
+++ add_mod.bash
@@ -6,8 +6,8 @@ SC_SCRIPT=${BASH_SOURCE[0]:-${0}}
 SC_TOP="$( cd -P "$( dirname "$SC_SCRIPT" )" && pwd )"
 
 module_url="$1";shift;
-target_path="$1";shift;
+module_name="$1";shift;
 
-git submodule add "$module_url" "plugins/start/${target_path}"
+git submodule add "$module_url/${module_name}" "plugins/start/${module_name}"
 
 exit
